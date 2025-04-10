aca06ef419c85eb453796749cd8f9c7ffbf5ab2e
lazynex <lazynex@gmail.com>
2025-04-10 20:10:46 +0300

    player class complete. asteroids class up to collisions

--- main.py
+++ main.py
@@ -1,27 +1,59 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame, sys
+
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            
-        screen.fill("black")
-
-
-
-
-        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # init
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    # player
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # asteroids
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    # game loop
+    while True:
+        # close window on press X
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+    
+        # update
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
+        for a in asteroids:
+            if a.collides(player):
+                print("Game over!")
+                sys.exit()
+
+        # flip screen
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
